c4faa8324b8338de80dd6c829d4eab3abc5b6c2d
kof.kofktu <kof.kofktu@kakaocorp.com>
2017-03-08 16:27:14 +0900

    라이브러리 및 샘플프로젝트 셋팅

---
+++ KUIKeyboard/KUIKeyboard.h
@@ -0,0 +1,19 @@
+//
+//  KUIKeyboard.h
+//  KUIKeyboard
+//
+//  Created by kofktu on 2017. 3. 8..
+//  Copyright © 2017년 Kofktu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KUIKeyboard.
+FOUNDATION_EXPORT double KUIKeyboardVersionNumber;
+
+//! Project version string for KUIKeyboard.
+FOUNDATION_EXPORT const unsigned char KUIKeyboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KUIKeyboard/PublicHeader.h>
+
+
